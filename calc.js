4ed69651473888c0e52cfcf80289d67232dbe4a1
Sophia Park <park.sophiakela@gmail.com>
2023-03-11 13:05:22 -0700

    All operations except square root working

--- calc.js
+++ calc.js
@@ -8,11 +8,29 @@ const reader = readline.createInterface({
 
 
 reader.question("What would you like to calculate?", function(input){
-	const tokens = input.split(' ');
+	tokens = input.split(' ');
 	
-	const mathSymbol = tokens[0];
-	const num1 = Number(tokens[1]);
+	const mathSymbol = tokens[1];
+	const num1 = Number(tokens[0]);
 	const num2 = Number(tokens[2]);
+
+	if (mathSymbol === "+"){
+		console.log(num1+num2);
+	}
+	 if (mathSymbol === "-"){
+		console.log(num1-num2);
+	}
+	if (mathSymbol === "*"){
+		console.log(num1*num2);
+	}
+	if (mathSymbol === "/"){
+		console.log(num1/num2);
+	}
+	if (mathSymbol === "-"){
+		console.log(num1-num2);
+	}
+
+	
 
 
 	// This line closes the connection to the command line interface.
